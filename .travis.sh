933f109db442e5e66c3b434593ad87f79097944f
Rik van der Heijden <rik.vanderheijden@dualinventive.com>
2016-04-16 19:56:34 +0200

    Fix build

--- .travis.sh
+++ .travis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#sudo apt-get update -qq || true
-#sudo apt-get install -qq -y --no-install-recommends libpam-dev
+sudo apt-get update -qq || true
+sudo apt-get install -qq -y --no-install-recommends libpam-dev
 
 if [[ "$CC" == "scan-build" ]]; then
     unset CC
     unset CXX
 
-    ./configure --prefix=/usr --with-openssl --with-pam_mods_dir=/lib/security --with-pam=/usr
+    scan-build -o analysis --status-bugs ./configure --prefix=/usr --with-openssl --with-pam_mods_dir=/lib/security --with-pam=/usr
     scan-build -o analysis --status-bugs make
 elif [[ "$CC" == "debian" ]]; then
     unset CC
