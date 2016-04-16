266fd4168d585565bdbfe9e1664ca1a5e82ab329
Rik van der Heijden <rik.vanderheijden@dualinventive.com>
2016-04-16 19:53:22 +0200

    Initial work on scan-build

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,19 @@
-sudo apt-get update -qq || true
-sudo apt-get install -qq -y --no-install-recommends libpam-dev
-./configure --prefix=/usr --with-openssl --with-pam_mods_dir=/lib/security --with-pam=/usr
-make
+#!/bin/bash
+#sudo apt-get update -qq || true
+#sudo apt-get install -qq -y --no-install-recommends libpam-dev
+
+if [[ "$CC" == "scan-build" ]]; then
+    unset CC
+    unset CXX
+
+    ./configure --prefix=/usr --with-openssl --with-pam_mods_dir=/lib/security --with-pam=/usr
+    scan-build -o analysis --status-bugs make
+elif [[ "$CC" == "debian" ]]; then
+    unset CC
+    unset CXX
+
+    debuild --no-lintian -us -uc -b
+else
+    ./configure --prefix=/usr --with-openssl --with-pam_mods_dir=/lib/security --with-pam=/usr
+    make
+fi
