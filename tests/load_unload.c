8205c7a606c179d3f147c3b18e5874afd78383c2
Rik van der Heijden <rik.vanderheijden@dualinventive.com>
2016-04-21 21:38:00 +0200

    It does some pam

--- tests/load_unload.c
+++ tests/load_unload.c
@@ -1,5 +1,14 @@
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
 #include <unity.h>
 #include <unity_fixture.h>
+
+extern int pam_sm_authenticate(pam_handle_t * pamh, int flags, int argc, const char **argv);
+
+const struct pam_conv conv = {
+	misc_conv,
+	NULL
+};
 
 TEST_GROUP(ProductionCode);
 
@@ -13,6 +22,17 @@ TEST_TEAR_DOWN(ProductionCode)
 
 TEST(ProductionCode, boembats)
 {
+	pam_handle_t *pamh;
+	int retval;
+
+	retval = pam_start("check_user", "nobody", &conv, &pamh);
+
+	// Are the credentials correct?
+	if (retval == PAM_SUCCESS) {
+		printf("Credentials accepted.\n");
+		retval = pam_sm_authenticate(pamh, 0, 0, NULL);
+	}
+
   TEST_ASSERT_EQUAL(0, 0);
 }
 
